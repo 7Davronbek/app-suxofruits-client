fad82bce7f48da1ccba1a6ff7ac34d0c914dcd69
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-10 00:52:33 +0500

    suxofruits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AboutUs from './components/AboutUs'
+import Galary from './components/Galary'
 import Header from './components/Header'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <Header />
       <AboutUs />
+      <Galary />
       <div className="vh-100"></div>
     </>
   )
---
+++ src/components/Galary.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Galary = () => {
+  return (
+    <>
+
+        <div className="galary">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-4 col-md-6">
+                        <div className="cards">
+                            <div className="img">
+                                <img src="/assets/image/galary/1.webp" className='w-100' alt="" />
+                            </div>
+                            <h2 className="bold">Сухофрукты</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    </>
+  )
+}
+
+export default Galary

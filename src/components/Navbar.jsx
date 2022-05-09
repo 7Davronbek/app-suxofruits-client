4249bd311ea73677ec728efe59b9f347dac8f3af
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-09 12:11:51 +0500

    suxofruits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Link } from 'react-router-dom'
@@ -12,6 +12,17 @@ const Navbar = () => {
                 <div className="row align-items-center">
                     <div className="col-1">
                         <div className="img"><img src="/assets/image/logo.png" className='w-100' alt="" /></div>
+                    </div>
+                    <div className="col-6 offset-2">
+                        <ul>
+                            <li><Link to='/' className='active'>Home</Link></li>
+                            <li><Link to='/' >History</Link></li>
+                            <li><Link to='/' >Service</Link></li>
+                            <li><Link to='/' >Gallary</Link></li>
+                        </ul>
+                    </div>
+                    <div className="col-3 ml-auto">
+                        <button className="btn btnGradient d-flex align-items-center"><span className="me-2"> <FontAwesomeIcon icon={faPhone} /> </span>Appointment me</button>
                     </div>
                 </div>
             </div>

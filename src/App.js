f829e3de9d4485ed752ec1df75fdb0f4c9f97879
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-09 23:46:20 +0500

    suxofruits

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import AboutUs from './components/AboutUs'
 import Header from './components/Header'
 
 const App = () => {
   return (
     <>
       <Header />
+      <AboutUs />
       <div className="vh-100"></div>
     </>
   )

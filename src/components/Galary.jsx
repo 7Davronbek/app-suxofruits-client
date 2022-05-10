c57b8b7bb3978609d81495c826869c55e5f9bed5
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-10 15:26:51 +0500

    suxofruits

--- src/components/Galary.jsx
+++ src/components/Galary.jsx
@@ -1,26 +1,105 @@
 import React from 'react'
 
 const Galary = () => {
-  return (
-    <>
+    return (
+        <>
 
-        <div className="galary">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-4 col-md-6">
-                        <div className="cards">
-                            <div className="img">
-                                <img src="/assets/image/galary/1.webp" className='w-100' alt="" />
+            <div className="galary">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-10 mx-auto">
+                            <div className="row">
+                                <div className="col-lg-4 col-md-6 mb-5">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/1.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Сухофрукты</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-5">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/2.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Орехи</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-5">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/3.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Бобовые</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-5">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/4.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">	Фрукты</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-5">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/5.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">СЛАДОСТИ</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6 mb-5">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/6.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Цукаты</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/1.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Сухофрукты</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/2.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Орехи</h2>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 col-md-6">
+                                    <div className="cards">
+                                        <div className="img">
+                                            <img src="/assets/image/galary/3.webp" className='w-100' alt="" />
+                                        </div>
+                                        <h2 className="bold">Бобовые</h2>
+                                    </div>
+                                </div>
+
                             </div>
-                            <h2 className="bold">Сухофрукты</h2>
                         </div>
                     </div>
                 </div>
+                
+                <div className="leftBottom"><img src="/assets/image/nuts.png" alt="" /></div>
             </div>
-        </div>
-    
-    </>
-  )
+
+        </>
+    )
 }
 
 export default Galary

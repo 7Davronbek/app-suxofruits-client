b80d047d4a5dd137786c557e35345014ef033d98
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-11 22:26:14 +0500

    suxofruits

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ContactsHeader from '../components/ContactsHeader'
 
 const Contacts = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1500);
+  }, []);
   return (
     <>
-    <ContactsHeader />
+      {isLoading ?
+        <>
+          <div className="loader">
+
+            <div id="wifi-loader">
+              <svg class="circle-outer" viewBox="0 0 86 86">
+                <circle class="back" cx="43" cy="43" r="40"></circle>
+                <circle class="front" cx="43" cy="43" r="40"></circle>
+                <circle class="new" cx="43" cy="43" r="40"></circle>
+              </svg>
+              <svg class="circle-middle" viewBox="0 0 60 60">
+                <circle class="back" cx="30" cy="30" r="27"></circle>
+                <circle class="front" cx="30" cy="30" r="27"></circle>
+              </svg>
+              <svg class="circle-inner" viewBox="0 0 34 34">
+                <circle class="back" cx="17" cy="17" r="14"></circle>
+                <circle class="front" cx="17" cy="17" r="14"></circle>
+              </svg>
+            </div>
+          </div>
+        </>
+        :
+        <>
+          <ContactsHeader />
+        </>
+      }
     </>
   )
 }

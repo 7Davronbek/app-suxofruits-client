dfdbaf35093cc4f3c6e98c98fb58df42aff1f288
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-21 19:33:52 +0500

    suxofruits

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,7 @@ import { Link, useLocation } from 'react-router-dom'
 const Navbar = () => {
     const [navbar, setNavbar] = useState(false);
     const location = useLocation();
+    const [burger, setBurger] = useState(false)
 
     const changeNavbar = () => {
         if (window.scrollY >= 30) {
@@ -22,22 +23,27 @@ const Navbar = () => {
             <div className={`navBar ${navbar ? 'active' : ''}`}>
                 <div className="container">
                     <div className="row align-items-center">
-                        <div className="col-2">
+                        <div className="col-lg-2 col-4">
                             <Link to='/' className="img"><h6 className=""><span className='bolds'>Eko</span>frukt</h6></Link>
                         </div>
-                        <div className="col-7 ">
-                            <ul>
-                                <li><Link to='/' className={`${location.pathname === '/' ? 'active' : ''}`}>Главная</Link></li>
-                                <li><Link to='/production' className={`${location.pathname === '/production' ? 'active' : ''}`} >Продукция</Link></li>
-                                <li><Link to='/about-company' className={`${location.pathname === '/about-company' ? 'active' : ''}`} >О компании</Link></li>
+                        <div className="col-10 mobileWrap ">
+                            <ul className={`${burger ? 'active' : ''}`}>
+                                <li><Link onClick={() => setBurger(false)} to='/' className={`${location.pathname === '/' ? 'active' : ''}`}>Главная</Link></li>
+                                <li><Link onClick={() => setBurger(false)} to='/production' className={`${location.pathname === '/production' ? 'active' : ''}`} >Продукция</Link></li>
+                                <li><Link onClick={() => setBurger(false)} to='/about-company' className={`${location.pathname === '/about-company' ? 'active' : ''}`} >О компании</Link></li>
                                 {/* <li><Link to='/about-company' className={`${location.pathname === '/about-company' ? 'active' : ''}`} >Польза сухофруктов</Link></li> */}
-                                <li><Link to='/photogalary' className={`${location.pathname === '/photogalary' ? 'active' : ''}`} >Фотогаларея</Link></li>
-                                <li><Link to='/contacts' className={`${location.pathname === '/contacts' ? 'active' : ''}`} >Контакты</Link></li>
+                                <li><Link onClick={() => setBurger(false)} to='/photogalary' className={`${location.pathname === '/photogalary' ? 'active' : ''}`} >Фотогаларея</Link></li>
+                                <li><Link onClick={() => setBurger(false)} to='/contacts' className={`${location.pathname === '/contacts' ? 'active' : ''}`} >Контакты</Link></li>
+                                <a href='tel: +998998190007 ' className="btn btnGradient2 ml-auto d-flex align-items-center"><span className="me-3"> <FontAwesomeIcon icon={faPhone} /> </span>Позвонить</a>
                             </ul>
                         </div>
-                        <div className="col-2 ml-auto">
-                            <a href='tel: +998998190007 ' className="btn btnGradient2 d-flex align-items-center"><span className="me-3"> <FontAwesomeIcon icon={faPhone} /> </span>Позвонить</a>
+
+                        <div onClick={() => setBurger(!burger)} className={`burger pr-4 ml-auto d-flex d-lg-none ${burger ? 'burgered' : ''}`}>
+                            <div className="burger1"></div>
+                            <div className="burger2"></div>
+                            <div className="burger3"></div>
                         </div>
+
                     </div>
                 </div>
             </div>

f04964fb94e464ee59b838f48e5defbd58a0addd
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-23 13:27:53 +0500

    suxofruits

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -29,7 +29,7 @@ const Companies = () => {
                     modules={[Autoplay, Navigation, Pagination]}
                     className="mySwiper"
                 >
-                    <SwiperSlide><h2 className="bold">Главные Агрофирма ООО «URGUT AGRO-VET SERVIS»  (для Сухофрукты) 2015-г</h2></SwiperSlide>
+                    <SwiperSlide><h2 className="bold">Главная Агрофирма ООО «URGUT AGRO-VET SERVIS»  (для Сухофрукты) 2015-г</h2></SwiperSlide>
                     <SwiperSlide><h2 className="bold">ООО «ZARBDOR UNIVERSAL LOGISTIKA»  2017-г (Гидропонная тепличная-ферма выращивает помидоры, огурцы  и другие свежую плодоовощную продукцию) </h2></SwiperSlide>
                     <SwiperSlide><h2 className="bold">ООО «ZARBDOR BARAKA AGROEKSPORT»  2020-г (для Сухофрукты)</h2></SwiperSlide>
                 </Swiper>

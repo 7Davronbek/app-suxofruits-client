d76eb6d0045d9bf5090985933f574d2ba2bd11ab
7Davronbek <maxmudovdavronbek8@gmail.com>
2022-05-24 14:25:22 +0500

    suxofruits

--- src/locales/UZ.js
+++ src/locales/UZ.js
@@ -27,7 +27,39 @@ export const uz = {
   why3: "HAMMA MAHSULOTLAR SERTIFIKATLANGAN VA XALQARO STANDARTLARGA MUVOFIQ",
   why4: "KENG ASSORTIMENT",
   why5: "UZLUKSIZ TA'MINOT",
-  why6: "DOIMIY MIJOZLAR UCHUN AJOYIB CHEGIRMALAR",
+  why6: "DOIMIY MIJOZLAR UCHUN AJOYIB CHEGIRMALAR SISTEMASI",
+
+  benefit1: "Salomatlik. Quritilgan mevalarning foydalari",
+  benefit2:
+    "Quritilgan mevalar haqiqiy qutqaruvchidir, ular deyarli har qanday taomni tayyorlashda ishlatilishi mumkin. Va agar siz ozgina tajriba o'tkazsangiz va quritilgan mevali idishni yong'oq bilan bezang, siz mazali, sog'lom va chiroyli bo'lasiz.",
+  benefit3: "Quritilgan mevalar haqida nimani bilishimiz kerak",
+  benefit4:
+    "Xurmo energiya manbai sifatida barcha quritilgan mevalardan ustundir. Ular E va H dan tashqari barcha vitaminlarni o'z ichiga oladi, lekin ular ayniqsa B5 ga boy, bu samaradorlikni oshiradi va konsentratsiyani oshiradi. Bundan tashqari, bu quritilgan mevalar tarkibida aspiringa o'xshash moddalar mavjud. Qadimgi tabiblar ularni shamollash va bosh og'rig'ini davolash uchun ishlatishlari tasodif emas.",
+  benefit5:
+    "Papayya oqsil almashinuvini faollashtiradi, immunitetni mustahkamlaydi, jinsiy faollikni oshiradi.",
+  benefit6:
+    "Armut tanadan og'ir metallar va toksinlarni olib tashlashga yordam beradi. U juda ko'p erimaydigan tolalarni o'z ichiga oladi, bu ichak faoliyatini normallashtiradi.",
+  benefit7:
+    "Olxo'ri balast moddalariga, shuningdek, B vitaminlariga boy.U uglevod almashinuvini optimallashtiradi, tashvishlarni yo'q qiladi va tananing stressli vaziyatlarga chidamliligini oshiradi.",
+  benefit8:
+    "Anjir qalqonsimon bez faoliyatini normallantiradi, shuningdek, saraton hujayralari rivojlanishining oldini oladi, bu so'nggi ilmiy tadqiqotlar bilan tasdiqlangan. Ovqat hazm qilishni rag'batlantiradigan fermentlarni o'z ichiga oladi. Xalq tabobatida u ichak parazitlarini olib tashlash va bronxitni davolashda muvaffaqiyatli qo'llaniladi.",
+  benefit9:
+    "Quritilgan o'rik, o'rik, kaisa kaliy, karotin, fosfor, kaltsiy, temir va B5 vitaminining ajoyib manbalari bo'lib, ular kuch berishdan tashqari, tanadagi yog'larni yoqishga ham yordam beradi. Bundan tashqari, quritilgan o'rikni muntazam iste'mol qilish saraton rivojlanish ehtimolini sezilarli darajada kamaytirishi ilmiy jihatdan isbotlangan.",
+  benefit10: "Yong'oqlardan birinchi yordam to'plami",
+  benefit11:
+    "Bodom: Shirin bodomlar yuqori xolesterin, gipertoniya, o'sma kasalliklari, ko'z kasalliklari, semirib ketish yaralari, o't yo'llari kasalliklari va yurak kuyishi uchun tavsiya etiladi.",
+  benefit12:
+    "Shakar bilan bodom yo'tal, astma, plevrit va hemoptizi uchun foydalidir; achchiq bodom ayollar kasalliklari, yuqori nafas yo'llari, buyraklar kasalliklarini davolashda ishlatiladi.",
+  benefit13:
+    "Fındık surunkali charchoq, diabet, gipertoniya, prostata kengayishi, varikoz tomirlari, tomirlarning yallig'lanishi (flebit) bilan yordam beradi. Yuqori kaloriya tarkibi, vitaminlar va mikroelementlarning keng doirasi tufayli u semirib ketgan bemorlarga ko'rsatiladi (katta hajmlar kerak emas, shuning uchun oshqozonni cho'zmaydi). Asal va quritilgan o'rik bilan birgalikda - yurak-qon tomir tizimi kasalliklarining oldini olish uchun ajoyib vosita. Mayiz bilan maydalangan findiq surunkali kamqonlikni davolash uchun ishlatiladi.",
+  benefit14:
+    "Kaju psoriaz, anemiya, depressiya, metabolik kasalliklar uchun tavsiya etiladi; immunitet tizimini mustahkamlaydi, yurakning normal faoliyatini ta'minlaydi; qon xolesterin darajasini pasaytirishga yordam beradi.",
+  benefit15:
+    "Pista tetiklantiruvchi, tonik va tiklovchi ta'sirga ega; yurak urish tezligini va yurak kasalliklariga moyillikni kamaytirish; miya, jigar faoliyatiga foydali ta'sir ko'rsatadi.",
+  benefit16:
+    "Ular katta jismoniy zo'riqish davrida va jiddiy kasalliklardan keyin yordam beradi. Arterial gipertenziya, surunkali anemiya, sil kasalligi, tromboflebit bilan og'rigan odamlar uchun foydalidir.",
+  benefit17:
+    "Yong'oq asab to'qimalari, yurak, jigar va boshqa organlarning faoliyatini yaxshilaydi; hujayralarning o'sishi va yangilanishiga yordam beradi, qondagi xolesterin darajasini pasaytiradi; diuretik sifatida ishlatiladi. Charchoq, uyqusizlik uchun foydali; xotira, e'tibor va eshitishni yaxshilaydi; potentsialni, libidoni oshiradi. Har kuni 30 gramm yeryong'oq iste'mol qilish yurak-qon tomir kasalliklari xavfini sezilarli darajada kamaytiradi.",
 
   companies1:
     "Asosiy Agrofirma ООО «URGUT AGRO-VET SERVIS» (Suxofrukt uchun) 2015-y",
